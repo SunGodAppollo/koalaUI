393088db84e57da57a6b1aa8066eaea7d910501d
Your Name <you@example.com>
2020-05-02 00:24:10 +0800

    添加导航

--- js/koala.js
+++ js/koala.js
@@ -6,7 +6,7 @@
   // adjust body font size
   function setBodyFontSize () {
     if (document.body) {
-      document.body.style.fontSize = (12 * dpr) + 'px'
+     // document.body.style.fontSize = (12 * dpr) + 'px'
     }
     else {
       document.addEventListener('DOMContentLoaded', setBodyFontSize)
